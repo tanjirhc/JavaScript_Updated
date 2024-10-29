77aec8c2c5c9a232673268a4f0e67ba714798ccf
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-29 23:48:55 +0600

    different mathmetical functions

--- variable.js
+++ variable.js
@@ -1,4 +1,4 @@
 console.log("10+15 = " + (10+15));
-console.log("10-15");
-console.log("10*15");
-console.log("10/15");
+console.log("10-15 = " + (10-15));
+console.log("10*15 = " + (10*15));
+console.log("10/15 = " + (10/15));
