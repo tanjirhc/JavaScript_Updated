f8988a5536ea065ea139115585fe9a7214079859
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-30 11:51:10 +0600

    naming convention

--- datatypes.js
+++ datatypes.js
@@ -2,8 +2,10 @@ let a = 10
 let b = 10.5
 let c = "Hello"
 let d = true
+let e = null;
 
 console.log(typeof a);
 console.log(typeof b);
 console.log(typeof c);
 console.log(typeof d);
+console.log(typeof e);
