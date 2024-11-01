d50d8dd6c72f865d1daaef1f8508a41d9493b9f0
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-01 21:18:18 +0600

    normal array print

---
+++ array-traverse.js
@@ -0,0 +1,11 @@
+/*
+! Article: Traverse Array Elements
+*/
+
+const arr = [1, 2, 3, 4, 5, 6];
+const n = arr[3];
+const m = arr[2];
+const x = 1,
+      y = 0;
+
+console.log(m, n, arr[y], arr[x]+arr[y], arr[x+y+1]);
