e2db2f12ec3817545b6f8334cc5d42adee99f1c2
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-01 12:41:17 +0600

    conditional statement

---
+++ conditional.js
@@ -0,0 +1,11 @@
+const theme = 'dark';
+let bgColor = theme === 'dark' ? '#333333' : '#efefef';
+
+console.log(bgColor);
+
+// if(theme === 'dark') {
+//   bgColor = '##333333';
+// }
+// else {
+//   bgColor = '#efefef';
+// }
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+if(theme == 'dark') {
+//   bgColor = '##333333';
+// }
+// else {
+//   bgColor = '#efefef';
+// }
