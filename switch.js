0daab7d66c1871c8367b02fdae0035df99ef8165
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-01 12:25:19 +0600

    switch case

---
+++ switch.js
@@ -0,0 +1,18 @@
+const day = 0;
+
+switch (day) {
+  case 0:
+    console.log('Monday');
+  case 1:
+    console.log('Sunday');
+  case 2:
+    console.log('Tuesday');
+  case 3:
+    console.log('Wednesday');
+  case 4:
+    console.log('Thursday');
+  case 5:
+    console.log('Friday');
+  case 6:
+    console.log('Saturday');
+}
