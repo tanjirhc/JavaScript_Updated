9f2bea3ded208d3a7f663c62b4dd7095a3ff308d
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-01 21:28:04 +0600

    Array Sum

--- array-traverse.js
+++ array-traverse.js
@@ -12,7 +12,22 @@ const x = 1,
 
 // Simple Traverse
 
-for (let i=0; i < arr.length; i++) {
+// for (let i=0; i < arr.length; i++) {
   
-  console.log(arr[i]);
+//   console.log(arr[i]);
+// }
+
+// Array Sum & Avg
+/**
+ * sum = 0
+ * sum = 0 + 1 = 1
+ * sum = 1+2 = 3
+ * sum = 3+3 = 6
+ * sum = 6+4 = 10
+ **/
+
+let sum = 0;
+for (let i=0; i < arr.length; i++) {  
+  sum += arr[i];  
 }
+console.log(sum);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,4 @@
-console.log(arr.length);
-// console.log(arr);
+for (let i=0; i < arr.length; i++) {
+  
+//   console.log(arr[i]);
+// }
