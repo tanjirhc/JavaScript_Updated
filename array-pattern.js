684b2bf8a868ea943def24acf5f86913840b0239
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-01 20:41:52 +0600

    new Array() running

--- array-pattern.js
+++ array-pattern.js
@@ -1,9 +1,11 @@
 /*
 ! Article: Initialize Array - Constructor Vs Factory
 */
+// Literal type --- []
 
 // Constructor Pattern
-new Array()
+const a1 = new Array();
+console.log(a1, a1.length);
 
 
 // Factory Pattern
