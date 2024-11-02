e7583638b3a2d6745f14bb52ceb4e7746d832746
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-02 20:28:11 +0600

    function calling

--- function/function.js
+++ function/function.js
@@ -1,18 +1,31 @@
 const arr1 = [4, 6, 10, 3, 18, 23, 12];
 const arr2 = [6, 11, 21, 4, 7, 27, 19];
 
-let max = arr1[0];
-for(let v of arr1) {
-  if (v > max) {
-    max = v;
-  }
-}
-console.log(max);
+// let max = arr1[0];
+// for(let v of arr1) {
+//   if (v > max) {
+//     max = v;
+//   }
+// }
+// console.log(max);
 
-let max2 = arr2[0];
-for(let f of arr2) {
-  if (f > max2) {
-    max2 = f;
+// let max2 = arr2[0];
+// for(let f of arr2) {
+//   if (f > max2) {
+//     max2 = f;
+//   }
+// }
+// console.log(max2);
+
+function findMax(arr) {
+  let max = arr[0];
+  for(let v of arr) {
+    if (v > max) {
+      max = v;
+    }
   }
+  return max;
 }
-console.log(max2);
+
+console.log(findMax(arr1));
+console.log(findMax(arr2));
