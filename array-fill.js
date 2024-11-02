90af73343dfa35ff8799e8016221328792f87934
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-02 14:54:03 +0600

    array mute

--- array-fill.js
+++ array-fill.js
@@ -21,4 +21,19 @@ names[1] = 'Shegufta Taranjum'
 // Fill array and Update
 const response = new Array(9);
 response.fill(undefined);
+for (let i=0; i < response.length; i++) {
+  const rand = Math.floor(Math.random() * 10 + 1);
+  response[i] = rand > 5 ? 'X' : 'O';
+}
 console.log(response);
+
+// Array is mutable
+function update(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = `${i + 1}. ${arr[i]}`;
+  }
+  return arr;
+}
+
+update(names);
+console.log(names);
