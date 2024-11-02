82a8042f0b2edff82750a3c9abad65ca06b920e6
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-02 14:22:06 +0600

    Auto Array filling

--- array-fill.js
+++ array-fill.js
@@ -6,4 +6,11 @@ const arr = new Array(10);
 for (let i=0; i < arr.length; i++) {
   arr[i] = false;
 }
-console.log(arr);
+//console.log(arr);
+
+// Array Fill
+const arr2 = new Array(10);
+arr2.fill(0);
+console.log(arr2);
+
+const names = ['']
