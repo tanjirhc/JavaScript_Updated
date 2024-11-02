521320aae49add681b9536967bbc1445dbc7d951
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-02 20:35:18 +0600

    function sum

---
+++ function/2-function.js
@@ -0,0 +1,7 @@
+function myFunction() { // Function declare
+  console.log('My first function');
+  const a = 10;
+  const b = 20;
+  console.log('Sum = ' + (a + b));
+}
+myFunction(); // function invoking
