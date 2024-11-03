9d53f06df69047ff49d6076732918856c57f17da
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-03 12:35:09 +0600

    Object

---
+++ Object/1-obj.js
@@ -0,0 +1,12 @@
+// What is Object ?
+// Object is a key value pair to store data
+
+const fName1 = 'HM';
+const lName1 = 'Nayem';
+const email1 = 'hmnayem@gmail.com';
+
+const fName2 = 'Tanjir';
+const lName2 = 'Hasan';
+const email2 = 'tanjirhc@gmail.com';
+
+const person = ['HM', 'Tanjir', 'Salvy'];
