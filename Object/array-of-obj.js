61a31298e3d63a2d0c0f0b610e926b9e5ca4db03
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-03 13:26:34 +0600

    Array of objects

---
+++ Object/array-of-obj.js
@@ -0,0 +1,18 @@
+const p1 = {
+  name: 'HM Nayem',
+  email: 'hmnayem@gmail.com',
+}
+
+const p2 = {
+  name: 'Tanjir Hasan',
+  email: 'tanjirhc@gmail.com',
+}
+
+const p3 = {
+  name: 'Segufta Yesmin',
+  email: 'segufta01@gmail.com',
+}
+
+const people = [p1, p2, p3];
+
+console.log(people);
