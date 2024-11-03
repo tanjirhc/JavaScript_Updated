3ceecaa6bcde7beab9cafc013f7163687e3e3b1b
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-03 10:10:42 +0600

    str.toUpperCase

--- function/callback.js
+++ function/callback.js
@@ -1,8 +1,17 @@
-const arr = [1, 2, 3];
+// const arr = [1, 2, 3];
 
-let sum = 0;
-arr.forEach((v) => {
-  //console.log('Value ', v*v);
-  sum += v;
+// let sum = 0;
+// arr.forEach((v) => {
+//   //console.log('Value ', v*v);
+//   sum += v;
+// });
+// console.log(sum);
+
+function toUpper(str, cb) {
+  const upper = str.toUpperCase();
+  cb(upper);
+}
+
+toUpper('Stack Learner', (str) => {
+  console.log(str);
 });
-console.log(sum);
