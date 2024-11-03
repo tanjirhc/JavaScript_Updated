07564d2ef241519831c8ec38cdbbb807a60dd7c2
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-03 13:00:57 +0600

    adding object in person2

---
+++ Object/3-add-remove.js
@@ -0,0 +1,20 @@
+// const o = {};
+
+// console.log(typeof o);
+
+const person1 = {};
+person1.firstName = 'HM'
+person1.lastName = 'Namyem'
+person1.email = 'hmnayem@gmail.com'
+
+console.log(person1);
+console.log(person1.email);
+
+const person2 = {
+  firstName: 'Tanjir',
+  lastName: 'Hasan',
+  email: 'tanjirhc@gmail.com',
+};
+person2.id = '1234';
+
+console.log(person2);
