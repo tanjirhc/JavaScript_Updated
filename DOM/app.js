59d4181659397bf04da170308a01b953551a0f04
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-03 20:29:46 +0600

    getElementByClassName

--- DOM/app.js
+++ DOM/app.js
@@ -1,3 +1,8 @@
-const title = document.getElementById('title');
-title.innerHTML = 'My custom Title changed by JavaScript';
-console.log(title);
+// const title = document.getElementById('title');
+// title.innerHTML = 'My custom Title changed by JavaScript';
+// console.log(title);
+
+const documents = document.getElementsByClassName('para');
+
+
+console.log(documents);
