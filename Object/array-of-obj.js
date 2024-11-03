cc700fc7a2d70c5ce8ac2af9e931319e6963e901
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-03 13:32:14 +0600

    arranging objects inside array [ ]

--- Object/array-of-obj.js
+++ Object/array-of-obj.js
@@ -1,19 +1,39 @@
-const p1 = {
-  name: 'HM Nayem',
-  email: 'hmnayem@gmail.com',
-}
+// const p1 = {
+//   name: 'HM Nayem',
+//   email: 'hmnayem@gmail.com',
+// }
 
-const p2 = {
-  name: 'Tanjir Hasan',
-  email: 'tanjirhc@gmail.com',
-}
+// const p2 = {
+//   name: 'Tanjir Hasan',
+//   email: 'tanjirhc@gmail.com',
+// }
 
-const p3 = {
-  name: 'Segufta Yesmin',
-  email: 'segufta01@gmail.com',
-}
+// const p3 = {
+//   name: 'Segufta Yesmin',
+//   email: 'segufta01@gmail.com',
+// }
 
-const people = [p1, p2, p3];
+//const people = [p1, p2, p3];
+
+// console.log(people[0].name);
+// console.log(people[1].name);
+
+const people = [
+  {
+    name: 'HM Nayem',
+    email: 'hmnayem@gmail.com',
+  },
+
+  {
+    name: 'Tanjir Hasan',
+    email: 'tanjirhc@gmail.com',
+  },
+
+  {
+    name: 'Segufta Yesmin',
+    email: 'segufta01@gmail.com',
+  },
+];
 
 console.log(people[0].name);
 console.log(people[1].name);
