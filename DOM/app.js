4dd580b7987867ce71adf4405223de9c41e66b57
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-04 19:13:26 +0600

    window.onload function

--- DOM/app.js
+++ DOM/app.js
@@ -1,20 +1,8 @@
-// const title = document.getElementById('title');
-// title.innerHTML = 'My custom Title changed by JavaScript';
-// console.log(title);
 
-// const paragraphs = document.getElementsByClassName('para');
+window.onload = function () {
+  main();
+}
 
-
-//console.log(paragraphs);
-
-// for (let v of paragraphs) {
-//   v.style.color = 'red';
-// }
-
-//console.log(document.getElementsByClassName('container'));
-
-const title = document.querySelector('#title');
-console.log(title);
-
-const paragraphs = document.querySelectorAll('.para');
-console.log(paragraphs);
+function main() {
+  alert('Hello World')
+}
