f1e3ad46eeadd9a7922382e894755e599fb36eb9
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-04 22:24:42 +0600

    li items appending

--- DOM/dom.js
+++ DOM/dom.js
@@ -4,5 +4,13 @@ window.onload = function () {
 }
 
 function main() {
-  
+  const input = document.getElementById('input-name');
+  const addBtn = document.getElementById('add-btn');
+  const nameList = document.getElementById('name-list');
+
+  addBtn.addEventListener('click', function () {
+    const li = document.createElement('li');
+    li.innerText = 'Dummy Text';
+    nameList.appendChild(li);
+  });
 }
